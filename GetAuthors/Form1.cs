adf3528eb523df856b1ed5076a69fad644c25d46
Trong Nghia <trongnghia206@gmail.com>
2011-10-09 10:10:15 +0700

    commit for Authors

---
+++ GetAuthors/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+using GetAuthors.OptionService;
+
+namespace GetAuthors
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OptionClient optionClient = new OptionClient();
+            MessageBox.Show("Tên Thành Viên Nhóm : " + optionClient.Getauthors().GroupName + "\nTên Project: " +
+                            optionClient.Getauthors().ProjectName);
+        }
+    }
+}
